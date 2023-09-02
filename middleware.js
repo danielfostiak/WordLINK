8f6cf71f5da45f0d5e3dc2ddf5862b9fdb5abe9e
Daniel <fostiakd@gmail.com>
2023-09-02 19:10:14 +0100

    middlewarefix

--- middleware.js
+++ middleware.js
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 
 // const url = "localhost:3000/";
-const url = process.env.NEXT_PUBLIC_APP_URL;
+// const url = process.env.NEXT_PUBLIC_APP_URL;
 
 export function middleware(req) {
   const url = req.nextUrl;
   const { pathname } = url;
 
   if (pathname.startsWith(`/api`)) {
-    if (!req.headers.get("referer")?.includes(url)) {
+    if (!req.headers.get("referer")?.includes("https://wordlink2.vercel.app")) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
   }
