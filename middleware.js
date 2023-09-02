8a36213b35036d132043f2c1aa8b04c6ef24d7d4
Daniel <fostiakd@gmail.com>
2023-09-02 05:40:03 +0100

    removed middleware

--- middleware.js
+++ middleware.js
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware(req) {
-  const url = req.nextUrl;
-  const { pathname } = url;
+// export function middleware(req) {
+//   const url = req.nextUrl;
+//   const { pathname } = url;
 
-  if (pathname.startsWith(`/api`)) {
-    if (!req.headers.get("referer")?.includes("http://localhost:3000")) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-  }
+//   if (pathname.startsWith(`/api`)) {
+//     if (!req.headers.get("referer")?.includes("http://localhost:3000")) {
+//       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+//     }
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)"],
+// };
