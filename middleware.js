195f3d1ea7289e14325aa7165eefc4526d8e4174
Daniel <fostiakd@gmail.com>
2023-09-02 05:50:53 +0100

    next env

--- middleware.js
+++ middleware.js
@@ -5,7 +5,9 @@ export function middleware(req) {
   const { pathname } = url;
 
   if (pathname.startsWith(`/api`)) {
-    if (!req.headers.get("referer")?.includes("https://wordlink2.vercel.app")) {
+    if (
+      !req.headers.get("referer")?.includes(process.env.NEXT_PUBLIC_APP_URL)
+    ) {
       return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
   }
